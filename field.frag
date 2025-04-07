792a234611fb0d297bdddd7b1738d48dd8aeb6ca
Demian Nezhdanov <nzhd.demian@gmail.com>
2025-04-07 01:07:40 -0300

    pupirki v1

--- field.frag
+++ field.frag
@@ -19,49 +19,131 @@
 
 
 
+//  uniform float u_time;
+//  uniform vec2 u_mouse;
+//  uniform vec2 u_resolution;
 
-float width = 0.0;
-float cellSize = 0.001;
+vec3 colorB = vec3(0.7647, 0.0, 0.1255);
+vec3 colorA =vec3(0.7647, 0.0, 0.0118);
 
-float grid(vec2 uv)
-{
-  float d = length(uv) ;
-float glow = 0.01;//1.0 - u_startTime/60.;
-      // glow = clamp(glow,-0.,1.0)/12.;
-    uv /= cellSize/2.0;
-width += smoothstep(0.0,0.05,d)/3.;
-float dVert = length(fract(uv.x ) - 0.5);
-float dHorz = length(fract(uv.y ) - 0.5);
-float grid = smoothstep(width + glow,width,dVert) + 
-                 smoothstep(width + glow,width,dHorz);
-                   d = smoothstep(0.1,0.0,d);
-                 return grid*d;
+
+float hash11( float n )
+ {
+return fract(sin(n)*43758.5453);
+ }
+ vec2 hash12(float i){
+    float x = fract(sin(i*123.123)*543.2);
+    float y = fract(sin(i*312.123)*345.2);
+    return vec2(x,y);
 }
+float noise11(float x) {
+    float fractX = fract(x);
+    float intX = floor(x);
+    float r0 = fract(sin(intX) * 43758.5453);
+    float r1 = fract(sin(intX + 1.0) * 43758.5453);
+    return mix(r0, r1, fractX);
+}
+
+vec2 eyes(vec2 uv, vec2 pos, float size, vec2 target, float intensity)
+{
+    uv -= 0.5;
+    uv.x *= u_resolution.x/u_resolution.y;
+    uv += 0.5;
+    vec2 posL = pos;
+    vec2 posR = pos;
+    posL.x -= size*5.;
+    float ldL = length(uv - posL);
+    posR.x += size*12.;
+    float rdL = length(uv - posR);
+    float ld = smoothstep(size + 0.01, size, ldL);
+    float rd = smoothstep(size + 0.01 , size, rdL);
+    float shape = ld + rd;
+    float tL = length(uv - posL + target*size);
+     tL = smoothstep(size/3. + intensity, size/3., tL);
+    float tR = length(uv - posR + target*size);
+     tR = smoothstep(size/3. + intensity, size/3., tR);
+
+     ld -= tL;
+          rd -= tR;
+
+    return vec2(ld + rd, shape);
+}
+float body(vec2 uv, vec2 pos,float seed){
+ 
+    uv -= 0.5;
+    uv.x *= u_resolution.x/u_resolution.y;
+    
+    uv += 0.5;
+       uv -= pos;
+	float angle = atan(uv.x,uv.y);
+    float ns = noise11(angle*(5.0 * (uv.y - 1.0 + seed/22.)) + hash11(u_time/1111112.)*2. + 1.*12.);
+    ns = noise11(50.0*ns - hash11(1.)*2.-1.*12. + length(pos)*112.);
+    uv -= 0.5;
+    uv.y *= 1.2;
+    uv += 0.5;
+float body = length(uv);
+body = smoothstep(0.21 + ns/7.,0.2 + ns/7.,body);
+
+
+return body;
+}
+
+
+vec3 unit(vec2 uv, vec2 pos , float size, vec2 target, vec3 bodyColor,float seed) {
+  uv -= 0.5;
+    uv *= 1.0 + seed/22. + 3.;
+uv += 0.5;
+   vec2 eyesCol  = eyes(uv, pos, 0.02, target, 0.01);
+
+   vec3 col = body(uv,pos,seed)*colorB;
+    col = mix(col, vec3(eyesCol.x), eyesCol.y);
+    //  col = eyesCol;
+    return col;
+}
+
 void main()
 {
-
-    // vec2 uv = fragCoord.xy / u_resolution.xy-0.5;
-    // uv.x *= u_resolution.x/u_resolution.y;
-    // float grid = grid(uv);
-    // vec3 col = vec3(grid);
-
-	 float col;
-    float t = u_time*.1;
-   vec2 uv = fragCoord.xy / u_resolution.xy-0.5;
-
-  float d = length(uv) ;
-   uv /=  12.;
-   
-        float factor = 1.5;
-        for(int i=0;i<9;i++)
-        {
-            uv *= -factor*factor;
-            uv += sin(uv.yx/factor * (t*112.+1.))/factor;
-            col += sin(uv.x-uv.y+col)+cos(uv.y-uv.x);
-        }
-    gl_FragColor = vec4(vec3(col/5.0),1.0);
+vec2 uv = fragCoord.xy / u_resolution.xy;
+vec2 eyesUv = fragCoord.xy / u_resolution.xy;
 
 
 
-    // gl_FragColor = vec4(col, 1.0);
+vec2 m = vec2(0.0);
+
+
+m = 1.0 - m;
+m -= 0.5;
+
+
+// uv *= 2.0;
+// uv = fract(uv-1.);
+vec3 col  = vec3(.0);
+
+
+uv -= 0.5;
+uv /= 0.25;
+// uv += 0.5;
+
+for(int i = 0; i < 12; i++){
+    m.x = hash11(float(int(u_time/2. + float(i) )) + noise11(u_time/111112.)) - 0.5;
+m.y = hash11(float(int(u_time/2.+ float(i))) - noise11(u_time/111112.)) - 0.5;
+    vec2 pos = hash12(float(i) ) + 0.5 ;
+    pos.x *= sin(u_time*1.1 + float(i) + noise11(u_time/2. + float(i)));
+    pos.x /= noise11(u_time/2. + float(i));
+    pos.y *= cos(u_time*1.1+ float(i)+ noise11(u_time/12. + float(i)));
+    pos.y /= noise11(u_time/2. + float(i));
+    pos = clamp(pos, -2.0, 1.7);
+      if(i == 0){
+       pos = vec2( 0.5);
+       pos *= 4.;
+    }
+    vec3 unt = unit(uv, pos, 0.001, m,colorB,float(i)/12.);
+  
+    
+    col += unt;
+}
+// vec2 pos = hash12(0.1234541);
+//  col += unit(uv, pos, 0.1, m,colorB);
+
+gl_FragColor = vec4(col,1.0);
 }
--- script.js
+++ script.js
@@ -4,40 +4,45 @@ import * as THREE from 'three';
 import { FirstPersonControls } from 'three/addons/controls/FirstPersonControls.js';
 
 let camera, controls, scene, renderer, audio;
-let mesh, geometry, sphere, time,startTime, frag, sfrag, svert, vert, material, sphereMaterial, clock;
-
-const worldWidth = 128, worldDepth = 128;
+let sphere, time, startTime, sfrag, svert, sphereMaterial, clock;
 
 init();
 animate();
 
 function init() {
-    time = (0.01);
-    startTime = (0.01);
+    time = 0.01;
+    startTime = 0.01;
+
     camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 20000);
-    camera.rotation.y =  - 0.025;
-	camera.rotation.x = -Math.PI / 2.1 - 0.1;
-    
+    camera.rotation.y = -0.025;
+    camera.rotation.x = -Math.PI / 2.1 - 0.1;
+
     const audioLoader = new THREE.AudioLoader();
-    var loader = new THREE.FileLoader();
+    const loader = new THREE.FileLoader();
     const listener = new THREE.AudioListener();
     camera.add(listener);
     const sound = new THREE.Audio(listener);
 
-    // Load a sound and set it as the Audio object's buffer
-    audioLoader.load('sounds/sound.ogg', function (buffer) {
-        sound.setBuffer(buffer);
-        sound.setLoop(true);
-        sound.setVolume(0.5);
-        sound.play();
-    });
+    function playSound() {
+        audioLoader.load('radio_this.mp3', function (buffer) {
+            sound.setBuffer(buffer);
+            sound.setLoop(true);
+            sound.setVolume(0.5);
+            sound.play();
+        });
 
-    loader.load('field.frag', function (data) { frag = data; runMoreIfDone(); });
-    loader.load('field.vert', function (data) { vert = data; runMoreIfDone(); });
-    loader.load('sphereField.frag', function (data) { sfrag = data; runMoreIfDone(); });
-    loader.load('sphereField.vert', function (data) { svert = data; runMoreIfDone(); });
+        // Remove the listener after the first interaction
+        // window.removeEventListener('click', playSound);
+        // window.removeEventListener('keydown', playSound);
+    }
+    playSound()
+    // Wait for user interaction before playing
+    // window.addEventListener('click', playSound);
+    // window.addEventListener('keydown', playSound);
+    loader.load('field.frag', function (data) { sfrag = data; runMoreIfDone(); });
+    loader.load('field.vert', function (data) { svert = data; runMoreIfDone(); });
 
-    var numFilesLeft = 4;
+    let numFilesLeft = 2;
     function runMoreIfDone() {
         --numFilesLeft;
         if (numFilesLeft === 0) {
@@ -47,27 +52,15 @@ function init() {
 }
 
 function more() {
-    material = new THREE.ShaderMaterial({
-        side: THREE.DoubleSide,
-        vertexShader: vert,
-        fragmentShader: frag,
-        uniforms: {
-            u_time: { value: 0.0 },
-            u_startTime: { value: 0.0 },
-            u_mouse: { type: "v2", value: new THREE.Vector2() },
-            u_resolution: { type: "v2", value: new THREE.Vector2() }
-        }
-    });
     sphereMaterial = new THREE.ShaderMaterial({
         side: THREE.DoubleSide,
         vertexShader: svert,
         fragmentShader: sfrag,
         uniforms: {
             u_time: { value: 0 },
-          
             u_mouse: { type: "v2", value: new THREE.Vector2() },
             u_resolution: { type: "v2", value: new THREE.Vector2() },
-			u_scale: { type: "f", value: 0.0 }
+            u_scale: { type: "f", value: 0.0 }
         }
     });
 
@@ -76,25 +69,12 @@ function more() {
     scene = new THREE.Scene();
     scene.background = new THREE.Color(0.0, 0.0, 0.0);
     scene.fog = new THREE.FogExp2(0xaaccff, 0.0007);
-    sphere = new THREE.SphereGeometry(4000, 66, 66);
-    geometry = new THREE.PlaneGeometry(1000, 1000, worldWidth - 1, worldDepth - 1);
-    geometry.rotateX(-Math.PI / 2);
 
-    const position = geometry.attributes.position;
-    position.usage = THREE.DynamicDrawUsage;
-    // camera.rotateX = Math.PI  + 0.6;
-
-    const texture = new THREE.TextureLoader().load('https://threejs.org/examples/textures/water.jpg');
-    texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
-    texture.repeat.set(5, 5);
-    texture.colorSpace = THREE.SRGBColorSpace;
-
-    const meshSph = new THREE.Mesh(sphere, sphereMaterial);
+    const sphereGeometry = new THREE.SphereGeometry(5000, 66, 66);
+    const meshSph = new THREE.Mesh(sphereGeometry, sphereMaterial);
     meshSph.materialIndex = 2;
-    mesh = new THREE.Mesh(geometry, material);
-    mesh.material = material;
-    scene.add(mesh);
     scene.add(meshSph);
+
     renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
@@ -121,47 +101,24 @@ function animate() {
 
 function render() {
     const delta = clock.getDelta();
-    startTime += 0.1;
-    // camera.rotation.x.value = startTime/1111.;
-    if (startTime > 60) {
-        time +=  startTime/1000;
-        if( camera.position.y < 10){
-            camera.position.y += 0.002 ;
+    startTime += 0.01;
+
+    // if (startTime > 60) {
+        // time += startTime / 1000;
+        if (camera.position.y < 10) {
+            camera.position.y += 0.002;
         }
-    }
-    
-    // time +=  0.1;
-    material.uniforms.u_startTime.value = startTime ;
-    sphereMaterial.uniforms.u_time.value = time / 112.;
-    sphereMaterial.uniforms.u_resolution = { type: "v2", value: new THREE.Vector2(window.innerWidth * 10, window.innerWidth * 10) };
-    material.uniforms.u_time.value = time / 112.;
-    material.uniforms.u_resolution = { type: "v2", value: new THREE.Vector2(window.innerWidth * 10, window.innerWidth * 10) };
-    const pos = geometry.attributes.position;
-	var waveSize = time/1112.;
-	if(camera.rotation.x > 1.35){
-		sphereMaterial.uniforms.u_scale.value += 0.001;
-	}else if(camera.rotation.x > 0.0){
+    // }
 
-		sphereMaterial.uniforms.u_scale.value -= 0.001;
+    sphereMaterial.uniforms.u_time.value = startTime ;
+    sphereMaterial.uniforms.u_resolution.value = new THREE.Vector2(window.innerWidth * 10, window.innerWidth * 10);
 
-	}
-	console.log("Camera Rotation:", );
-	if(waveSize > 1.0) {
-		waveSize = 1.0;
-	}
-
-    for (let i = 0; i < pos.count; i++) {
-        var y = 0.0;
-        if (startTime > 6) {
-            y = 0.0;
-        }else{
-            y = 15 * (Math.sin((time / 12 + i / 512)) )*waveSize;
-        }
-         
-        pos.setY(i, y - 10*waveSize);
+    if (camera.rotation.x > 1.35) {
+        sphereMaterial.uniforms.u_scale.value += 0.01;
+    } else if (camera.rotation.x > 0.0) {
+        sphereMaterial.uniforms.u_scale.value -= 0.01;
     }
 
-    pos.needsUpdate = true;
     controls.update(delta);
     renderer.render(scene, camera);
 }
